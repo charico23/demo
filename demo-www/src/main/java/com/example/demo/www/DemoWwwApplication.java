181708da9f1add045caa330d5f3f118ddd514520
charico23 <fumie@wakamiya-no-MacBook-Pro.local>
2017-06-04 18:24:54 +0900

    test

---
+++ demo-www/src/main/java/com/example/demo/www/DemoWwwApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWwwApplication.class, args);
+	}
+}
---
+++ demo-www/src/test/java/com/example/demo/www/DemoWwwApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWwwApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
